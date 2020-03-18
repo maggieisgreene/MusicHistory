edc775d72060db1c88dcc15cd4a373e23f9a162f
maggieisgreene <maggie.greene96@gmail.com>
2020-03-18 14:21:24 -0700

    inital commit

---
+++ SQLIntro.sql
@@ -0,0 +1,20 @@
+﻿--SELECT * FROM Genre
+
+--INSERT INTO Song (Title, SongLength, AlbumId, ArtistId, GenreId, ReleaseDate) VALUES ('By Design', 77, 12, 12, 2, '11/01/1998')
+
+--SELECT * FROM Song
+
+--DELETE Song Where Id = 22
+
+--SELECT * FROM Song
+
+--INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Kid Cudi', 2001)
+
+--SELECT * FROM Artist
+
+--SELECT * FROM Album
+
+--INSERT INTO Album (Title, AlbumLength, ReleaseDate, [Label], ArtistId, GenreId)
+--VALUES ('Indicud', 10000, '11/01/02', 'Your Moms', 22, 1)
+
+--SELECT * FROM Album
